d048dfef22948f2d7457d1ef592673cff8bece4d
LDP <lukedanpenney@gmail.com>
2025-01-02 14:28:15 +0200

    Github Pages changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-});
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production'
+        ? '/Portfolio-Website-3D/'
+        : '/',
+    plugins: [react()]
+  }
+})
